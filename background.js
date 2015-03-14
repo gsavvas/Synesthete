b5d185284859638dcaccdb8b6b9cef722fe400a9
AlexCLeduc <ledualex@gmail.com>
2015-03-14 16:01:33 -0400

    Add arbitrary string (incl unicode) support

--- background.js
+++ background.js
@@ -1,42 +1,25 @@
-if (!localStorage['saved']){
-  localStorage['letA'] = '#800000'; //maroon
-  localStorage['letE'] = '#008000'; //green
-  localStorage['letI'] = '#0000ff'; //blue
-  localStorage['letO'] = '#008080'; //teal
-  localStorage['letU'] = '#800080'; //purple
-  
+(function(){
+if (!localStorage.letBlocks){
+	var item = Array(5); 
+	item[0] = {str: 'A', clr: '#800000'}; //maroon
+	item[1] = {str: 'E',clr: '#008000'}; //green
+	item[2] = {str: 'I', clr: '#0000ff'}; //blue
+	item[3] = {str: 'O', clr:'#008080'}; //teal
+	item[4] = {str:'U',clr: '#800080'}; //purple
+  localStorage["letBlocks"] = JSON.stringify(item);
   localStorage['saved'] = 'Y';
 
 }
 
-
-// Converts an integer (unicode value) to a char
-function itoa(i)
-{ 
-   return String.fromCharCode(i);
-}
-
-// Converts a char into to an integer (unicode value)
-function atoi(a)
-{ 
-   return a.charCodeAt();
-}
-
-
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if (request.method == "getColors"){
-		var colorList = new Array();
-		//first the letters
-		for( x=0; x<26; x++){
-			colorList[x] = localStorage['let' + itoa( atoi('A') + x) ];
-		}
-		//now the numbers...
-		for( x=26; x<36; x++){
-			colorList[x] = localStorage['let' + itoa( atoi('0') + x - 26) ];
-		}
-		sendResponse({colors: colorList});
-	  }
-    else{
+		var letBlocks = JSON.parse(localStorage.letBlocks);
+
+		sendResponse(letBlocks);
+	  } else{
 		sendResponse({}); // snub them.
 	  }
 });
+
+
+})();
--- swap.js
+++ swap.js
@@ -1,50 +1,21 @@
-// Converts an integer (unicode value) to a char
-function itoa(i)
-{ 
-   return String.fromCharCode(i);
-}
+(function(){
 
-// Converts a char into to an integer (unicode value)
-function atoi(a)
-{ 
-   return a.charCodeAt();
-}
-
-//just an array of number to letter mappings
-var letters = new Array()
-for( x=0; x<26; x++){
-  letters[x] = itoa( atoi('a') + x);
-}
-
-for( x=26; x<36; x++){
-  letters[x] = itoa( atoi('0') + x - 26);
-}
-
-//get array of letters and their corresponding colors
+//This is just to access the localStorage and return the list of string/color pairs.
 chrome.extension.sendRequest({method: "getColors"}, function(response) {
   //console.log(response.colors);
 
-  for( x=0; x<26; x++){
-    var let = itoa( atoi('A') + x);
-    var mlet = "let" + let;
-    //document.write('letter:' + let+ ' the stored result is: ' + localStorage[mlet] + "<br>" );
-    if(response.colors[x]){
-      $('body').highlight(let,response.colors[x]);
-  }
-  
-}
+  console.log(response);
 
-  for( x=26; x<36; x++){
-    var let = itoa( atoi('0') + x - 26);
-    var mlet = "let" + let;
-	//console.log("index:" + x + " char:" + let + " color:" + response.colors[x] );
-    //document.write('letter:' + let+ ' the stored result is: ' + localStorage[mlet] + "<br>" );
-    if(response.colors[x]){
-      $('body').highlight(let,response.colors[x]);
-  }
-  
-} 
-  
-  
+
+  response.forEach(function(elm){
+    $('body').highlight(elm.str, elm.clr);
+  });
   
 });
+
+
+
+
+
+
+})();
